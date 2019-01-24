13a73e5fa9afe5727c8319ed7817c991d0299ba8
root <root@ik-dw3-G33T-AM.h015110.net>
2019-01-24 14:17:14 +0100

    view de ofertas con cards en add.blade.php

--- resources/views/Add.blade.php
+++ resources/views/Add.blade.php
@@ -1,13 +1,75 @@
 @extends('Layouts.master')
 
 @section('content')
-
-    <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
-            {{ csrf_field() }}
-            Choose your xls/csv File : <input type="file" name="file" class="form-control">
-         
-            <input type="submit" class="btn btn-primary btn-lg" style="margin-top: 3%">
+    <!-- <div class="row">
+        <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data" class="col-12">
+                {{ csrf_field() }}
+                Choose your xls/csv File : <input type="file" name="file" class="form-control">
+             
+                <input type="submit" value="Gehitu Ikasleak" class="btn btn-primary btn-lg" style="margin-top: 3%">
         </form>
+    </div> -->
+    <div class="row">
+
+        <div class="col-lg-3">
+          <div class="list-group filtroak">
+            <a href="#" class="list-group-item">Category 1</a>
+            <a href="#" class="list-group-item">Category 2</a>
+            <a href="#" class="list-group-item">Category 3</a>
+          
+              <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data" class="list-group-item">
+                    {{ csrf_field() }}
+                    Aukeratu zeure xlsx/xls/csv artxiboa : <input type="file" name="file" class="form-control">
+                 
+                    <input type="submit" value="Gehitu Ikasleak" class="btn btn-primary btn-lg" style="margin-top: 3%">
+            </form>
+            </div>
+        </div>
+        <div class="col-lg-9">
+            <div class="row">
+
+                <div class="col-lg-4 col-md-8 mb-4 carta">
+                  <div class="card h-100">
+                    <div class="card-header">
+                            <ul class="nav nav-tabs card-header-tabs">
+                              <li class="nav-item">
+                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Enpresa</a>
+                                </li>
+                              <li class="nav-item">
+                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                        </li>
+                            </ul>
+                            </div>
+
+                    <div class="card-body">
+                      <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                          <h4 class="card-title">
+                            Enpresaren Izena
+                          </h4>
+                          
+                          <p class="card-text">Deskripzioa:  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                          <p class="card-text">Noiz arte</p>
+                        </div>
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                          <p class="card-text">Departamentua</p>
+                          <p class="card-text">Lan postua</p>
+                          <p class="card-text">Plaza hutsak</p>
+                        
+                        </div>
+                      
+                    </div>
+                    <div class="card-footer">
+                        <small><button id="bidaliCV" class="btn btn-primary btn-lg">Bidali CV</button></small>
+                      <!-- <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small> -->
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
 
 @stop
    
--- resources/views/Layouts/master.blade.php
+++ resources/views/Layouts/master.blade.php
@@ -27,8 +27,8 @@
     @include('Partials.footer')
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!--<script src="{{ asset('js/jquery.min.js') }}" ></script>
+    <script src="{{ URL::asset('js/jquery.min.js') }}" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>-->
+    <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
   </body>
 </html>
