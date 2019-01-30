96c77d55232dbfae38a70f81116fadb98ba28f0a
root <root@ik-dw3-G33T-AM.h015110.net>
2019-01-30 09:55:47 +0100

    arreglos administratzailea

--- app/Http/Controllers/AdministratzaileaController.php
+++ app/Http/Controllers/AdministratzaileaController.php
@@ -58,28 +58,24 @@ class AdministratzaileaController extends Controller
         return view('administratzaileaIrakaslea');
     }
 
-    public function create()
-    {
-        return view('create');
+    
+    public function IrakasleakSortu(){
+        return view('administratzaileaIrakasleaSortu');
     }
+    public function insert(Request $request)
+    {     
+        $Administratzailea= new administratzaileak();
+        $Administratzailea->email= $request['email'];
+        $Administratzailea->pasahitza= $request['pasahitza'];
+        $Administratzailea->mota= $request['mota'];
+        $Administratzailea->egoera= $request['egoera'];
+        $Administratzailea->departamentua= $request['departamentua'];
 
-    public function store()
-    {
-        $this->validate(request(), [
-            
-            'email' => 'required|email',
-            'pasahitza' => 'required',
-          	 'mota' => 'required',
-          	 'egoera' => 'required',
-            'departamentua' => 'required'
-        ]);
-      
-        
-        $Administratzaileak = Administratzaileak::create(request([ 'email', 'pasahitza','mota','egoera','departamentua']));
-        
-        
-        
-        return redirect()->to('/create');
+    // add other fields
+    $Administratzailea->save();
+
+
+        return redirect('/administratzailea/irakasleak/sortu');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -62,10 +62,10 @@ Route::post('create', 'AdministratzaileaController@store');
 
     Route::get('/administratzailea', 'AdministratzaileaController@Index');
     
-
+   
     Route::get('/administratzailea/irakasleak', 'AdministratzaileaController@Irakasleak');
 
-    
+    Route::post('insert', 'AdministratzaileaController@insert')->name('insert');
     Route::get('/administratzailea/irakasleak/sortu', 'AdministratzaileaController@IrakasleakSortu');
     
     Route::get('/administratzailea/ikasleak', 'AdministratzaileaController@Ikasleak');
