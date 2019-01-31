cc8732cd812beaddf4cc16bcb4c259ebf17fad28
root <root@ik-dw3-G33T-AM.h015110.net>
2019-01-31 13:18:24 +0100

    diseño y jquery para CV

--- resources/views/ikasleaCV.blade.php
+++ resources/views/ikasleaCV.blade.php
@@ -11,87 +11,81 @@
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="fname" name="name" type="text" placeholder="{{$perfila['izena']}}" class="form-control">
+                                <input id="fname" name="name" type="text"  class="form-control">
                             </div>
                         </div>
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="lname" name="name" type="text" placeholder="{{$perfila->apellidos}}" class="form-control">
+                                <input id="lname" name="name" type="text"  class="form-control">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="email" name="email" type="text" placeholder="{{$perfila->email}}" class="form-control">
+                                <input id="email" name="email" type="text"  class="form-control">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$perfila['dni']}}" class="form-control">
+                                <input id="phone" name="phone" type="text"  class="form-control">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$perfila['jaiotze_Data']}}" class="form-control">
+                                <input id="phone" name="phone" type="text"  class="form-control">
                             </div>
                         </div>
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$perfila['helbidea']}}" class="form-control">
+                                <input id="phone" name="phone" type="text"  class="form-control">
                             </div>
                         </div>
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$perfila['kontaktua']}}" class="form-control">
+                                <input id="phone" name="phone" type="text"  class="form-control">
                             </div>
                         </div>
-                        @foreach ($titulazioak)
-                        <div class="form-group">
+                       <div class="form-group">
+                            <!--Titulazioak-->
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
-                            <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$titulazioa['titu']}}" class="form-control">
+                            <div class="col-md-8" id="TextBoxesGroup1">
+                                
+                                <div id="TextBoxTitulazioa1">
+                                    <input name="titulazioa1" type="text"  class="form-control" id="titulazioa1"><br/>
+                                    
+                                </div>
+                                
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$titulazioa['titu']}}" class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
-                            <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$titulazioa['titu']}}" class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
-                            <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$titulazioa['titu']}}" class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
-                            <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$titulazioa['titu']}}" class="form-control">
-                            </div>
-                        </div>
-                        @endforeach
-                        <div class="form-group">
-                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
-                            <div class="col-md-8">
-                                <input id="phone" name="phone" type="text" placeholder="{{$titulazioa['titu']}}" class="form-control">
-                            </div>
-                        </div>
 
+                                <input type='button' value='Titulazioa gehitu' id='addButton' class="btn btn-primary btn-lg">
+                                        
+                                <input type='button' value='Titulazioa kendu' id='removeButton' class="btn btn-primary btn-lg"> 
+                            </div>
+                            
+                        </div>
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
+                            <div class="col-md-8" id="TextBoxesGroup2">
+                                <!--HIzkuntzak-->
+                                <div id="TextBoxHizkuntza1">
+                                    <input id="hizkuntza1" name="hizkuntza1" type="text" class="form-control"><br/>
+                                    <input id="maila1" name="maila1" type="text"  class="form-control"><br/>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <input type='button' value='Hizkuntza gehitu' id='addButton1' class="btn btn-primary btn-lg">
+                                <input type='button' value='Hizkuntza kendu' id='removeButton1' class="btn btn-primary btn-lg">
+                            </div>
+                        </div>
                         <div class="form-group">
                             <div class="col-md-12 text-center">
                                 <button type="submit" class="btn btn-primary btn-lg">Gorde</button>
