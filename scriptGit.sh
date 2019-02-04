c8229e39b3b4799ca97ee6f983e9fdc02c99309e
root <>
2019-02-04 11:54:27 +0000

    04/02/2019

--- scriptGit.sh
+++ scriptGit.sh
@@ -8,23 +8,16 @@ echo "escribe el directorio donde lo quieras guardar"
 read DIRECTORIO
 echo "$DIRECTORIO"
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 45b843b670f422b4765789966b5b9fe9aea24728
 if [ -d "$DIRECTORIO" ]
 then
    echo "El directorio ${DIRECTORIO} existe"
    cd $DIRECTORIO
    git pull origin master
    git add .
-<<<<<<< HEAD
    git commit -m "$DIA"
 	echo " $DIA -  $HORA"
-=======
    git commit -m "$DIA" 
     echo "$DIA - $HORA"
->>>>>>> 45b843b670f422b4765789966b5b9fe9aea24728
    git push -u origin master
    
    
@@ -36,11 +29,8 @@ else
     git pull origin master
     git add .
     git commit -m "$DIA"
-<<<<<<< HEAD
 	echo " $DIA -  $HORA"
-=======
 echo "$DIA"
->>>>>>> 45b843b670f422b4765789966b5b9fe9aea24728
     git push -u origin master 
 fi
 
