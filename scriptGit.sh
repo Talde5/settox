3415cfdb3b21afba8b24bc3f4a5e2fc43badbf37
root <>
2019-02-04 09:41:41 +0000

    04/02/2019

--- scriptGit.sh
+++ scriptGit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DIRECTORIO=/home/ik_2dw3/Escritorio/pepe
+DIRECTORIO=/var/www/html/settox
 DIA=`date +"%d/%m/%Y"`
 
 
 if [ -d "$DIRECTORIO" ]
 then
    echo "El directorio ${DIRECTORIO} existe"
-   cd /home/ik_2dw3/Escritorio/pepe
+   cd /var/www/html/settox
    git pull origin master
    git add .
    git commit -m "$DIA"
@@ -17,8 +17,8 @@ then
    
 else
     echo "El directorio ${DIRECTORIO} no existe"
-    git clone https://github.com/Talde5/settox /home/ik_2dw3/Escritorio/pepe
-    cd /home/ik_2dw3/Escritorio/sep
+    git clone https://github.com/Talde5/settox /var/www/html/settox
+    cd /var/www/html/settox
     git pull origin master
     git add .
     git commit -m "$DIA"
